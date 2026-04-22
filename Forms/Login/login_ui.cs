8e3d12475788e5789278025daaf3f0c478af334a
Dadanchii <dhanreighatienza@gmail.com>
2026-04-22 12:07:41 +0800

    Implement login lockout after multiple failed attempts
    Added lockout mechanism for failed login attempts.

--- Forms/Login/login_ui.cs
+++ Forms/Login/login_ui.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -14,10 +14,55 @@ namespace StudyQuest
         private string defaultUsername = "admin";
         private string defaultPassword = "1234";
 
+        private int failedAttempts = 0;
+        private const int maxAttempts = 3;
+        private const int lockoutSeconds = 30;
+        private System.Windows.Forms.Timer? lockoutTimer;
+        private int remainingSeconds;
+
         public login_ui()
         {
             InitializeComponent();
             passwordTextbox.UseSystemPasswordChar = true;
+            InitializeLockoutTimer();
+        }
+
+        private void InitializeLockoutTimer()
+        {
+            lockoutTimer = new System.Windows.Forms.Timer();
+            lockoutTimer.Interval = 1000;
+            lockoutTimer.Tick += LockoutTimer_Tick;
+        }
+
+        private void LockoutTimer_Tick(object? sender, EventArgs e)
+        {
+            remainingSeconds--;
+
+            loginButton.Text = $"Wait {remainingSeconds}s...";
+
+            if (remainingSeconds <= 0)
+            {
+                lockoutTimer!.Stop();
+                failedAttempts = 0;
+                loginButton.Enabled = true;
+                loginButton.Text = "Login";
+                usernameTextbox.Enabled = true;
+                passwordTextbox.Enabled = true;
+                usernameTextbox.Focus();
+            }
+        }
+
+        private void StartLockout()
+        {
+            remainingSeconds = lockoutSeconds;
+            loginButton.Enabled = false;
+            loginButton.Text = $"Wait {remainingSeconds}s...";
+            usernameTextbox.Enabled = false;
+            passwordTextbox.Enabled = false;
+            lockoutTimer!.Start();
+
+            MessageBox.Show($"Too many failed attempts. Please wait {lockoutSeconds} seconds before trying again.",
+                "Account Locked", MessageBoxButtons.OK, MessageBoxIcon.Warning);
         }
 
         private void studyQuestLogo_Click(object sender, EventArgs e)
@@ -32,25 +77,38 @@ namespace StudyQuest
 
         private void usernameTextbox_TextChanged(object sender, EventArgs e)
         {
-           
+
         }
+
         private void button1_Click(object sender, EventArgs e)
         {
             if (usernameTextbox.Text == defaultUsername && passwordTextbox.Text == defaultPassword)
             {
+                failedAttempts = 0;
                 new dashboard_ui().Show();
                 this.Hide();
             }
             else
             {
-                MessageBox.Show("The Username or Password you enter is incorrect, try again");
-                usernameTextbox.Clear();
-                passwordTextbox.Clear();
-                usernameTextbox.Focus();
+                failedAttempts++;
+
+                int attemptsLeft = maxAttempts - failedAttempts;
+
+                if (failedAttempts >= maxAttempts)
+                {
+                    usernameTextbox.Clear();
+                    passwordTextbox.Clear();
+                    StartLockout();
+                }
+                else
+                {
+                    MessageBox.Show($"Incorrect username or password. {attemptsLeft} attempt(s) remaining.",
+                        "Login Failed", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    usernameTextbox.Clear();
+                    passwordTextbox.Clear();
+                    usernameTextbox.Focus();
+                }
             }
         }
-          
-        
     }
 }
-
